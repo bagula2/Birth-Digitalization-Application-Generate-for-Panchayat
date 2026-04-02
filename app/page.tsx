c204d887001f0575ebf526a1b99da18058525a6a
bagula2 <nitush6488@gmail.com>
2026-04-03 03:09:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div style={{padding:20}}>
-      <h1>Bagula Birth Certificate Application</h1>
-      <p>⏱ Takes 3–5 minutes</p>
-      <p>Apply Once, Submit Once</p>
-    </div>
-  );
+  return <h1>Working</h1>;
 }
